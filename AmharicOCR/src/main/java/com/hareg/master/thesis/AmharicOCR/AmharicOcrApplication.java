326d6123f1966a9e4adb9a91ba195116f2fa07c1
Yonas <Yonas@XPS-DEV-1>
2020-05-11 00:44:22 +0100

    Initial Commit For this Project : Created Spring Boot Project and Configure the Application Properties

---
+++ AmharicOCR/src/main/java/com/hareg/master/thesis/AmharicOCR/AmharicOcrApplication.java
@@ -0,0 +1,13 @@
+package com.hareg.master.thesis.AmharicOCR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmharicOcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmharicOcrApplication.class, args);
+	}
+
+}
---
+++ AmharicOCR/src/test/java/com/hareg/master/thesis/AmharicOCR/AmharicOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hareg.master.thesis.AmharicOCR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmharicOcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
